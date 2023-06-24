a815588b439df1be7c3d020c4e7c29ce40eb9d67
codewithnimraa <136242525+codewithnimraa@users.noreply.github.com>
2023-06-24 17:32:54 +0500

    Add files via upload

---
+++ Q4.list.dart
@@ -0,0 +1,6 @@
+void main() {
+  List<int> num1 = [360, 330, 320, 980];
+  num1.sort();
+  print("Smallest value in the list : ${num1.first}");
+  print("Largest value in the list : ${num1.last}");
+}
